51d8a134bbb7947d8b991f752b017e62ffa5e18f
Anubhav404error <susanewatson7777@gmail.com>
2025-08-03 23:01:12 +0530

    apps.py

--- employee/apps.py
+++ employee/apps.py
@@ -1,21 +1,24 @@
-"""
-apps.py
-"""
-
 from django.apps import AppConfig
+import sys
 
 
 class EmployeeConfig(AppConfig):
     """
     AppConfig for the 'employee' app.
-
-    This class represents the configuration for the 'employee' app. It provides
-    the necessary settings and metadata for the app.
-
-    Attributes:
-        default_auto_field (str): The default auto field to use for model field IDs.
-        name (str): The name of the app.
     """
 
     default_auto_field = "django.db.models.BigAutoField"
     name = "employee"
+
+    def ready(self):
+        # Import signals if any (optional)
+        from employee import signals
+
+        # Only start scheduler if not running management commands
+        if not any(cmd in sys.argv for cmd in ['makemigrations', 'migrate', 'collectstatic', 'shell', 'loaddata', 'createsuperuser']):
+            try:
+                from employee.scheduler import run_disciplinary_scheduler, scheduler  # or whatever your function is called
+                if not scheduler.running:
+                    scheduler.start()
+            except Exception as e:
+                print("Error starting employee scheduler:", str(e))
